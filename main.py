1557b3e50388b717b3e6d6964d5d03203ef97952
Maulen <zharyq2k21@gmail.com>
2021-05-09 13:02:31 +0600

    Добавлено новые методы

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from rich import print
 from rich.prompt import Prompt
+import time
 import os
 
 class zharTools(object):
@@ -13,21 +14,24 @@ class zharTools(object):
       
       # Наш баннер и меню
       print(
-         "[green bold]    _            _____         _    \n"
-         " __| |_  __ _ _ |_   _|__  ___| |___\n"
-         "|_ / ' \/ _` | '_|| |/ _ \/ _ \ (_-<\n"
-         "/__|_||_\__,_|_|  |_|\___/\___/_/__/[/green bold]\n"         
-         "\n"
-         " [[green bold]1[/green bold]] [#eeeeee]Bruteforce[/#eeeeee]\n"
-         " [[green bold]2[/green bold]] Phishing\n"
-         " [[green bold]3[/green bold]] Help\n"
-         "\n"
-         " [[red bold]0[/red bold]] Exit\n"
-         ""
+         "[green bold]"
+         "\t    _            _____         _    \n"
+         "\t __| |_  __ _ _ |_   _|__  ___| |___\n"
+         "\t|_ / ' \/ _` | '_|| |/ _ \/ _ \ (_-<\n"
+         "\t/__|_||_\__,_|_|  |_|\___/\___/_/__/\n"
+         "[/green bold]"
+         "\t\n"
+         "\t[[green bold]1[/green bold]] [#eeeeee]Bruteforce[/#eeeeee]\n"
+         "\t[[green bold]2[/green bold]] Phishing\n"
+         "\t[[green bold]3[/green bold]] Author\n"
+         "\t\n"
+         "\t[[red bold]0[/red bold]] Exit\n"
+         "\t"
       )
 
    def get_command(self):
-        return self.prompt.ask(" [[green]*[/green]] Команда")
+      # Спрашиваем команду и вернем ее в функцию 
+      return self.prompt.ask("\t[[green]*[/green]] Command")
 
    def clear_terminal(self):
       if os.sys.platform == "nt":
@@ -39,8 +43,20 @@ class zharTools(object):
       self.clear_terminal() # Очищаем терминал
 
       # Сообщение об неправильности команды
-      print(" [[red bold]-[/red bold]] Команда [red]'{0}'[/red] не найдено!\n".format(command))
-      self.prompt.ask(" [[yellow]![/yellow]] Нажмите Enter")
+      print(
+         "\n[bold yellow]"
+         "\t █████ █████     █████    █████ █████ \n"
+         "\t░░███ ░░███    ███░░░███ ░░███ ░░███  \n"
+         "\t ░███  ░███ █ ███   ░░███ ░███  ░███ █\n"
+         "\t ░███████████░███    ░███ ░███████████\n"
+         "\t ░░░░░░░███░█░███    ░███ ░░░░░░░███░█\n"
+         "\t       ░███░ ░░███   ███        ░███░\n"
+         "\t       █████  ░░░█████░         █████\n"
+         "\t      ░░░░░     ░░░░░░         ░░░░░  \n"
+         "[/bold yellow]"
+      )
+      print("\t[[red bold]-[/red bold]] Command [red]'{0}'[/red] not found!\n".format(command))
+      self.prompt.ask("\t[[yellow]![/yellow]] Press Enter")
 
       # Заново показываем меню
       self.menu()
@@ -48,8 +64,37 @@ class zharTools(object):
    def exit_program(self):
       self.clear_terminal() # Очищаем терминал
 
-      print(" [[yellow]![/yellow]] Выходим из программы[white]...[/white]\n")
+      print(
+         "[bold red]"
+         "\t ___     _ _    \n"   
+         "\t| __|_ _(_) |_  \n" 
+         "\t| _|\ \ / |  _| \n"
+         "\t|___/_\_\_|\__| \n"
+         "[/bold red]"
+         "\n"
+         "\t[[yellow]![/yellow]] Exit the program[white]...[/white]\n"
+      )
+      # Ждем 2 секунды и закрывем программу
+      time.sleep(2)
       exit()
+
+   def author(self):
+      self.clear_terminal()
+      print(
+         "[bold blue]"
+         "\t   _       _   _            \n"            
+         "\t  /_\ _  _| |_| |_  ___ _ _ \n" 
+         "\t / _ \ || |  _| ' \/ _ \ '_|\n"
+         "\t/_/ \_\_,_|\__|_||_\___/_|  \n"
+         "[/bold blue]\n"
+         "\t[[bold blue]*[/bold blue]] Author: Maulen Azikulov\n"
+         "\t[[bold blue]*[/bold blue]] Instagram: @zhar_yq\n"
+         "\t[[bold blue]*[/bold blue]] Telegram: @zharyqtyq\n"
+      )
+      self.prompt.ask("\t[[green bold]*[/green bold]] Press Enter")
+      
+      # Вернемся к начальному меню
+      self.menu()
 
    def menu(self):
       self.banner()
@@ -59,8 +104,8 @@ class zharTools(object):
          print("BruetForce")
       elif comm == "2": # Вход в раздел Фишинг
          print("Phishing")
-      elif comm == "3": # Вход в раздел Помощь
-         print("Help")
+      elif comm == "3": # Вход в раздел Автор
+         self.author()
       elif comm == "0": 
          self.exit_program() # Выйти с программы
       else: 
